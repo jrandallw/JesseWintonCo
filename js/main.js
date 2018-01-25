034c14f463a1e837d1d96eab0b5598963980440c
Jesse Winton <hi@jessewinton.co>
2018-01-25 13:24:11 -0800

    updates

--- js/main.js
+++ js/main.js
@@ -6,46 +6,9 @@ Filename: main.js
 $(document).ready(function(){var t=new TimelineMax;
   t.from(".description",.5,{opacity:0},0);
   t.staggerFrom(".proposal-overview > div",.5,{opacity:0},0.1);
+  t.from(".intro .content",.5,{opacity:0},.5);
   t.staggerFrom(".info ul li",.5,{opacity:0},0.1);
   t.staggerFrom(".content-block, .project",.5,{opacity:0},0.1);
-});
-
-// proposals
-// Select all links with hashes
-$('a[href*="#"]')
-// Remove links that don't actually link to anything
-.not('[href="#"]')
-.not('[href="#0"]')
-.click(function(event) {
-  // On-page links
-  if (
-    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-    && 
-    location.hostname == this.hostname
-  ) {
-    // Figure out element to scroll to
-    var target = $(this.hash);
-    target = target.length ? target : $('[id=' + this.hash.slice(1) + ']');
-    // Does a scroll target exist?
-    if (target.length) {
-      // Only prevent default if animation is actually gonna happen
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000, function() {
-        // Callback after animation
-        // Must change focus!
-        var $target = $(target);
-        $target.focus();
-        if ($target.is(":focus")) { // Checking if the target was focused
-          return false;
-        } else {
-          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-          $target.focus(); // Set focus again
-        };
-      });
-    }
-  }
 });
 
 // greensock
