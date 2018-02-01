7165b4874b0a0f90d71eace3add5e9e2f83d762d
Jesse Winton <hi@jessewinton.co>
2018-01-31 23:11:17 -0800

    general updates

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
   
   window.tl = new TimelineLite();
   
-  tl.from(".description",.5,{opacity:0},0);
+  tl.from(".hero, .description",.5,{opacity:0},0);
   tl.staggerFrom(".proposal-overview > div, .project-overview .container > div",.5,{opacity:0},0.1);
   tl.staggerFrom(".info ul li",.5,{opacity:0},0.1);
   tl.staggerFrom(".content-block",.5,{opacity:0},0.1);
