ef78d66a8641e2bcc6c3d237ec4a1148654e12e9
Jesse Winton <hi@jessewinton.co>
2018-03-17 10:51:04 -0700

    updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".project-index-grid").imagesLoaded(function() {
+    /*$(".project-index-grid").imagesLoaded(function() {
         $(".project-index-grid").isotope({
             itemSelector: ".project",
             percentPosition: !0,
@@ -8,14 +8,14 @@ $(document).ready(function(){
                 columnWidth: ".grid-sizer"
             }
         })
-    })
+    })*/
     
     window.tl = new TimelineLite();
     tl.from(".hero, .description",.5,{opacity:0},0);
+    tl.staggerFrom(".project",.5,{opacity:0, ease: Power1.easeOut},0.1);
     tl.staggerFrom(".proposal-overview > div, .project-overview .container > div",.5,{opacity:0},0.1);
     tl.staggerFrom(".info ul li",.5,{opacity:0},0.1);
     tl.staggerFrom(".content-block",.5,{opacity:0},0.1);
-    tl.staggerFrom(".project",.5,{opacity:0, ease: Power1.easeOut},0.1);
   
     $(".inquire-form").validetta({
       display: "inline",
