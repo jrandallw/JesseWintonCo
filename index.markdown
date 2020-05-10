a250fad8ae04875c42e3a0b408c69aecd8248772
Jesse Winton <hi@jessewinton.co>
2020-05-10 01:21:08 -0700

    updates to masonry grid

--- index.markdown
+++ index.markdown
@@ -1,5 +1,5 @@
 ---
-title: Home
+title: Index
 date: '2017-09-08T20:14:00.000+00:00'
 layout: index
 hero_image: ''
--- js/main.js
+++ js/main.js
@@ -1,21 +1,8 @@
-$(window).load(function(){
-  
-    
-    window.tl = new TimelineLite();
-    //tl.staggerFrom(".index .heading",1,{opacity:0},0.5);
-    tl.staggerFrom(".photo",.5,{opacity:0},0.1);
+$(document).ready(function(){
+    $('.project-index-grid').masonry({
+        columnWidth: 70,
+        itemSelector: '.project'
+    });
 
-   
-    /*$(".photos-index-grid").imagesLoaded(function() {
-      $(".photos-index-grid").isotope({
-          itemSelector: ".photo",
-          percentPosition: !0,
-          layoutMode: "masonry",
-          masonry: {
-              columnWidth: ".grid-sizer"
-          }
-      })
-  })*/
-  
 
-  });
+});
