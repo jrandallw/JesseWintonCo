3f493612615d7fe194c99ebc59af57f841847c01
Jesse Winton <hi@jessewinton.co>
2020-05-13 13:44:39 -0700

    updating sourcing

---
+++ src/_site/js/app.js
@@ -0,0 +1,19 @@
+import barba from '@barba/core';
+import { delay, pageTransition, contentAnimation } from "./modules/transitions.js";
+import regeneratorRuntime from "regenerator-runtime";
+
+barba.init({
+  sync: true,
+  transitions: [{
+    name: 'swipe-transition',
+    async leave(data) {
+      const done = this.async();
+      pageTransition();
+      await delay(1000);
+      done();
+    },
+    async enter(data) { 
+      contentAnimation(); 
+    }
+  }]
+});
