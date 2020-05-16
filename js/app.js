3ab7de603dba3170c2f6a072f00acb48d0bc41f6
Jesse Winton <hi@jessewinton.co>
2020-05-16 13:27:30 -0700

    updating plugins

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 import barba from '@barba/core';
+import 'babel-polyfill';
 import { delay, pageTransition, workIndexAnimation } from "./modules/transitions.js";
-import regeneratorRuntime from "regenerator-runtime";
 
 barba.init({
   transitions: [{
